46e2fa081feedc0cab1942dc0f6788079d67c352
jaynopponep <noppone.p@gmail.com>
2024-09-08 16:42:20 -0400

    submissions are validated, problems are stored by id

--- main.py
+++ main.py
@@ -8,21 +8,56 @@ bot = commands.Bot(command_prefix="lc ", intents=discord.Intents.all())
 with open("token.txt") as file:
 	token = file.read()
 
+def extract_problem(link):
+	try:
+		if "/submissions" in link and "/problems/" in link:
+			problem = link.split("/problems/")[1].split("/submissions")[0]
+		else:
+			return None
+		return problem
+	except IndexError:
+		return None
+
 @bot.command()
-async def submit(ctx, arg):
+async def submit(ctx, link):
 	try:
 		if os.path.exists('users.json'):
 			with open('users.json', 'r', encoding='utf8') as f:
-				user = json.load(f)
+				users = json.load(f)
 		else:
-			user = {}
-		if str(ctx.author.id) in user:
-			user[str(ctx.author.id)]['submissions'] += 1
+			users = {}
+		problem_name = extract_problem(link)
+		# read users & problem name^
+
+		if problem_name is None:
+			await ctx.send("Wrong link format, resubmit the link that contains the submission ID")
+			return
+		# handle submission, retrieve problem name
+
+		if str(ctx.author.id) in users:
+			users[str(ctx.author.id)]['submissions'] += 1
+			if 'links' in users[str(ctx.author.id)] and 'problems' in users[str(ctx.author.id)]:
+				users[str(ctx.author.id)]['links'].append(link)
+				if problem_name in users[str(ctx.author.id)]['problems']:
+					await ctx.send("Cannot submit the same problem more than once! Nice try Diddy")
+					return
+				else:
+					users[str(ctx.author.id)]['problems'].append(problem_name)
+			elif 'links' not in users[str(ctx.author.id)]:
+				users[str(ctx.author.id)]['links'] = [link]
+			else:
+				users[str(ctx.author.id)]['problems'] = [problem_name]
 		else:
-			user[str(ctx.author.id)] = {'submissions': 1}
+			users[str(ctx.author.id)] = {
+				'submissions': 1,
+				'links': [link],
+				'problems': [problem_name]
+			}
+		# handle new data in json^
+
 		with open('users.json', 'w', encoding='utf8') as f:
-			json.dump(user,f,sort_keys=True,indent=4,ensure_ascii=False)
-		subs = user[str(ctx.author.id)]['submissions']
+			json.dump(users,f,sort_keys=True,indent=4,ensure_ascii=False)
+		subs = users[str(ctx.author.id)]['submissions']
 		await ctx.channel.send(f"{ctx.author.name} now has {subs} submissions")
 	except Exception as e:
 		await ctx.send(f"An error occurred: {e}")
