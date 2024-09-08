deb5f734f6c473353cebaed5bd0abaf33af139ab
jaynopponep <noppone.p@gmail.com>
2024-09-08 15:47:14 -0400

    leaderboard function works, still need validation on links

--- main.py
+++ main.py
@@ -27,4 +27,18 @@ async def submit(ctx, arg):
 	except Exception as e:
 		await ctx.send(f"An error occurred: {e}")
 
+@bot.command()
+async def leaderboard(ctx):
+	try:
+		embed = discord.Embed(title="Leaderboard", description="", color=discord.Color.random())
+		leaderboard = ""
+		with open('users.json', 'r', encoding='utf8') as f:
+			users = json.load(f)
+		for user_id, score in users.items():
+			user = await bot.fetch_user(int(user_id))
+			leaderboard += f"{user.name} - {score['submissions']}\n"
+		embed.add_field(name="Highest Leetcode Submissions (all time)", value=leaderboard, inline=False)
+		await ctx.channel.send(embed=embed)
+	except Exception as e:
+		await ctx.send(f"Error occurred: {e}")
 bot.run(token)
